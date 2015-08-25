35ba6dee82a7f1ab7684cc32fb52fea7dc07f359
poppy <poppy@gmail.com>
2015-08-25 20:05:51 +0800

    1

---
+++ Demo2_second/Demo2_second/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo2_second
+//
+//  Created by tarena on 15/8/25.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"day01");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
